2bc2d11fc7872b2726b039b0f1170b62000b6963
Chris <christopher.cahill@aol.com>
2015-07-10 11:25:20 +0200

    adding email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,14 +42,14 @@ Rails.application.configure do
    # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
+#   config.paperclip_defaults = {
+#   :storage => :s3,
+#   :s3_credentials => {
+#     :bucket => ENV['S3_BUCKET_NAME'],
+#     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+#     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+#   }
+# }
 
   # change to true to allow email to be sent during development
   config.action_mailer.raise_delivery_errors = true
